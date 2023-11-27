ea7ae6e82842c2ecc1f8a7969ade166efa2ca822
Taylor <taylorlauck%@gmail.com>
2023-11-26 19:42:06 -0500

    second commit

---
+++ src/Login.js
@@ -0,0 +1,45 @@
+// Login.js (or any relevant component name)
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/adduser', {
+        username,
+        password,
+      });
+      console.log(response.data); // Assuming the server sends a response
+      // You might want to redirect the user after successful login.
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
