6be8bb20c38cfb1b10abef3a387d862994fd7488
Taylor <taylorlauck%@gmail.com>
2023-12-26 18:41:40 -0500

    idk

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const { Pool } = require ("pg");
 
   const pool = new Pool({
     user: "postgres",
-    password: "joel1968",
+    password: "shhhh",
     host: "localhost",
     port: 5432,
     database: "capstone",
